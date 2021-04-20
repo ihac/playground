9a55ba888939fccd2f9dea0a3c4a9c82401a86d5
Luffy Xiao <luffy.xiao@hotstar.com>
2021-04-20 20:08:11 +0800

    Support recursive query
    Signed-off-by: Luffy Xiao <luffy.xiao@hotstar.com>

--- graphql-server-example/src/index.ts
+++ graphql-server-example/src/index.ts
@@ -2,12 +2,20 @@ import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
   type Book {
-    title: String
-    author: String
+    title: String!
+    author: Author!
+  }
+
+  type Author {
+    name: String!
+    books: [Book!]
   }
 
   type Query {
-    books: [Book]
+    books: [Book!]
+    authors: [Author!]
+    bookByTitle(title: String!): Book
+    author(name: String!): Author
   }
 `;
 
@@ -22,13 +30,35 @@ const books = [
   },
   {
     title: "One Piece",
-    author: "Luffy Xiao",
+    author:  "Luffy Xiao",
+  },
+  {
+    title: "One Piece II",
+    author:"Luffy Xiao",
   },
 ];
 
+const authors = [
+  {
+    name: 'Kate Chopin',
+  },
+  {
+    name: 'Paul Auster',
+  },
+  {
+    name: 'Luffy Xiao',
+  },
+]
+
 const resolvers = {
+  Author: {
+    books: (parent: any) => books.filter(b => b.author === parent.name)
+  },
   Query: {
     books: () => books,
+    authors: () => authors,
+    bookByTitle: (parent: any, args: any, context: any, info: any) => books.find(b => b.title === args.title),
+    author: (parent: any, args: any, context: any, info: any) => authors.find(a => a.name === args.name),
   },
 };
 
